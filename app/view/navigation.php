2fc884dfdd8e5cab4168209f31e781a52ea3d848
MahiraS <smahira8@vt.edu>
2019-04-23 18:57:34 -0400

    HTML Canvas
    added HTML canvas element to navigation page to hold a temporary map
    image, but it still needs work-- its very blurry.

--- app/view/navigation.php
+++ app/view/navigation.php
@@ -1,3 +1,4 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="home2">Campus Map</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,13 +25,15 @@
   </div>
 </nav>
 
+
 <h1>Navigation</h1>
 
+<!--
 <p>This will be the hardest obstacle on this route:</p>
 <div class="barrierpic" style = 'background-image:"<?= BASE_URL ?>/public/road.jpg"; style="background-size: 100% 100%"'>
 
-<!--img src= "<?= BASE_URL ?>/public/road.jpg" alt="dangerous road" style="background-size: 100% 100%"--><!--source: https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiXyZiX1KDhAhVCiFkKHfrpCiIQjRx6BAgBEAU&url=https%3A%2F%2Fwww.tripadvisor.co.nz%2FLocationPhotoDirectLink-g154972-d186150-i154850860-Cabot_Trail-Cape_Breton_Island_Nova_Scotia.html&psig=AOvVaw0nA6X6G9AvlhLRM3U6QlpC&ust=1553718905259933-->
-
+<img src= "<?= BASE_URL ?>/public/road.jpg" alt="dangerous road" style="background-size: 100% 100%"--><!--source: https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiXyZiX1KDhAhVCiFkKHfrpCiIQjRx6BAgBEAU&url=https%3A%2F%2Fwww.tripadvisor.co.nz%2FLocationPhotoDirectLink-g154972-d186150-i154850860-Cabot_Trail-Cape_Breton_Island_Nova_Scotia.html&psig=AOvVaw0nA6X6G9AvlhLRM3U6QlpC&ust=1553718905259933-->
+<!--
 </div>
 <button type="submit" style = "margin-top: 15px;" class="btn btn-primary btn-lg btn-block mb-2">Let's go!</button>
 <button type="submit" style = "margin-top: 15px;" class="btn btn-primary btn-lg btn-block mb-2">Try Alternative Route</button>
@@ -39,3 +42,62 @@
   <p>time to arrive:  </p>
   <p>Arrive at:  </p>
 </div>
+
+-->
+<!--
+Response:
+{
+path:[
+{x: ,
+ y: ,
+map_id: },
+{x: ,
+ y: ,
+map_id: },
+{x: ,
+ y: ,
+map_id: },
+...]
+}
+-->
+
+
+<canvas id="samplemap">
+<img id= "map" src= "<?= BASE_URL ?>/public/map_sample.JPG" alt="map">
+</canvas>
+<img id= "map2" src= "<?= BASE_URL ?>/public/map_sample.JPG" alt="map" width="500" height="600">
+
+<style> canvas { width: 1000px; height: 1000px; border: 1px solid red; }</style>
+
+<script type="text/javascript">
+
+window.onload = function() {
+  var canvas = document.getElementById("samplemap");
+  var ctx = canvas.getContext("2d");
+  var img = document.getElementById("map");
+  var hRatio = canvas.width / img.width;
+  var vRatio = canvas.height / img.height;
+  var ratio  = Math.min ( hRatio, vRatio );
+  //ctx.drawImage(img, 0, 0);
+  //ctx.drawImage(img, 0,0, img.width, img.height, 0,0,img.width*ratio, img.height*ratio);
+// ctx.drawImage(img, 0, 0, img.width,    img.height,     // source rectangle
+//                     0, 0, canvas.width, canvas.height); // destination
+
+var centerShift_x = ( canvas.width - img.width*ratio ) / 2;
+   var centerShift_y = ( canvas.height - img.height*ratio ) / 2;
+   ctx.clearRect(0,0,canvas.width, canvas.height);
+   ctx.drawImage(img, 0,0, img.width, img.height,
+                      centerShift_x,centerShift_y,img.width*ratio, img.height*ratio);
+
+  context.beginPath();
+  // Staring point (10,45)
+    context.moveTo(10,45);
+    // End point (180,47)
+  context.lineTo(180,47);
+    // Make the line visible
+    context.stroke();
+};
+
+
+
+</script>
